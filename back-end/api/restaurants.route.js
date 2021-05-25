e33c7f82f58338551eb3d13cdd9d8bd18998612a
Tifenn <tifenn.queudet@gmail.com>
2021-05-25 16:25:01 +0200

    reviews controller with different methods

--- back-end/api/restaurants.route.js
+++ back-end/api/restaurants.route.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import RestaurantCtrl from "./restaurants.controller.js"
+import ReviewsCtrl from "./reviews.controller.js"
 
 const router = express.Router()
 
 router.route('/').get(RestaurantCtrl.apiGetRestaurants)
+
+router
+    .route('/review')
+    .post(RewiewsCtrl.apiPostreview)
+    .put(RestaurantCtrl.apiUpdateReview)
+    .delete(RestaurantCtrl.apiDeleteReview)
 
 export default router
