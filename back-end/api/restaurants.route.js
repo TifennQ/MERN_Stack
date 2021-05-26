5bb07a3e4f440166fcf5b1372e57516fd065e5d8
Tifenn <tifenn.queudet@gmail.com>
2021-05-26 13:52:22 +0200

    add getByid and cuisines in restaurant controller

--- back-end/api/restaurants.route.js
+++ back-end/api/restaurants.route.js
@@ -5,11 +5,9 @@ import ReviewsCtrl from "./reviews.controller.js"
 const router = express.Router()
 
 router.route('/').get(RestaurantCtrl.apiGetRestaurants)
+router.route('/id/:id').get(RestaurantCtrl.apiGetRestaurantById)
+router.route('/cuisines').get(RestaurantCtrl.apiGetRestaurantCuisines)
 
-router
-    .route('/review')
-    .post(RewiewsCtrl.apiPostreview)
-    .put(RestaurantCtrl.apiUpdateReview)
-    .delete(RestaurantCtrl.apiDeleteReview)
+router.route('/review').post(ReviewsCtrl.apiPostReview).put(ReviewsCtrl.apiUpdateReview).delete(ReviewsCtrl.apiDeleteReview)
 
 export default router
