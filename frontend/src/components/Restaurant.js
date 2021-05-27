0825fe18e57ac757a78234ccccf682615d68a5da
Tifenn <tifenn.queudet@gmail.com>
2021-05-27 10:02:21 +0200

    Restaurants Component : setting the functions

--- frontend/src/components/Restaurant.js
+++ frontend/src/components/Restaurant.js
@@ -1,6 +1,91 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import RestaurantDataService from "../services/restaurant";
+import { Link } from "react-router-dom";
 
-const restaurant = () => {
+
+const Restaurant = (props) => {
+    const [restaurants, setRestaurants] = useState([])
+    const [searchName, setSearchName] = useState("")
+    const [searchZip, setSearchZip] = useState("")
+    const [searchCuisine, setSearchCuisine] = useState("")
+    const [cuisines, setCuisines] = useState("")
+
+    useEffect(() => {
+        retrieveRestaurants()
+        retrieveCuisines()
+    }, [])
+
+    const onChangeSearchName = e => {
+        const searchName = e.target.value
+        setSearchName(searchName)
+    }
+
+    const onChangeSearchZip = e => {
+        const searchZip = e.target.value
+        setSearchZip(searchZip)
+    }
+
+    const onChangeSearchCuisine = e => {
+        const searchCuisine = e.target.value
+        setSearchCuisine(searchCuisine)
+    }
+
+    const retrieveRestaurants = () => {
+        RestaurantDataService.getAll().then(
+            response => {
+                console.log(response.data)
+                setRestaurants(response.data.restaurants)
+            }
+        )
+        .catch(e => {
+            console.log(e);
+        })
+    }
+
+    const retrieveCuisines = () => {
+        RestaurantDataService.getCuisines().then(
+            response => {
+                console.log(response.data);
+                setCuisines(["All Cuisines"].concat(response.data))
+            }
+        )
+        .catch(e => {
+            console.log(e);
+        })
+    }
+
+    const refreshList = () => {
+        retrieveRestaurants()
+    }
+
+    const find = (query, by) => {
+        RestaurantDataService.find(query, by).then(
+            response => {
+                console.log(response.data);
+                setRestaurants(response.data.restaurants)
+            }
+        )
+        .catch(e => {
+            console.log(e);
+        })
+    }
+
+    const findByName = () => {
+        find(searchName, "name")
+    }
+
+    const findByZip = () => {
+        find(searchZip, "zipcode")
+    }
+
+    const findByCuisine = () => {
+        if(searchCuisine == "All Cuisines") {
+            refreshList()
+        } else {
+            find(searchCuisine, "cuisine")
+        }
+    }
+
     return (
         <div>
            Restaurant 
@@ -8,4 +93,4 @@ const restaurant = () => {
     )
 }
 
-export default restaurant
+export default Restaurant
