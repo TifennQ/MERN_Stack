eaacd7a6ac3e81b41597e618787682d58f4a69df
Tifenn <tifenn.queudet@gmail.com>
2021-05-27 12:30:47 +0200

    Restaurant.js setting functions

--- frontend/src/components/Restaurant.js
+++ frontend/src/components/Restaurant.js
@@ -4,176 +4,46 @@ import { Link } from "react-router-dom";
 
 
 const Restaurant = (props) => {
-    const [restaurants, setRestaurants] = useState([])
-    const [searchName, setSearchName] = useState("")
-    const [searchZip, setSearchZip] = useState("")
-    const [searchCuisine, setSearchCuisine] = useState("")
-    const [cuisines, setCuisines] = useState("")
+   const initialRestaurantState = {
+       id: null,
+       name: "",
+       address: {},
+       cuisine: "",
+       reviews: []
+   }
+   const [restaurant, setRestaurant] = useState(initialRestaurantState)
 
-    useEffect(() => {
-        retrieveRestaurants()
-        retrieveCuisines()
-    }, [])
+   const getRestaurant = id => {
+       RestaurantDataService.get(id).then(
+           (response => {
+               setRestaurant(response.data)
+               console.log(response.data);
+           })
+       )
+       .catch(e => {
+           console.log(e);
+       })
+   }
 
-    const onChangeSearchName = e => {
-        const searchName = e.target.value
-        setSearchName(searchName)
-    }
+   useEffect(() => {
+       getRestaurant(props.match.params.id)
+   }, [props.match.params.id])
 
-    const onChangeSearchZip = e => {
-        const searchZip = e.target.value
-        setSearchZip(searchZip)
-    }
-
-    const onChangeSearchCuisine = e => {
-        const searchCuisine = e.target.value
-        setSearchCuisine(searchCuisine)
-    }
-
-    const retrieveRestaurants = () => {
-        RestaurantDataService.getAll().then(
-            response => {
-                console.log(response.data)
-                setRestaurants(response.data.restaurants)
-            }
-        )
-        .catch(e => {
-            console.log(e);
-        })
-    }
-
-    const retrieveCuisines = () => {
-        RestaurantDataService.getCuisines().then(
-            response => {
-                console.log(response.data);
-                setCuisines(["All Cuisines"].concat(response.data))
-            }
-        )
-        .catch(e => {
-            console.log(e);
-        })
-    }
-
-    const refreshList = () => {
-        retrieveRestaurants()
-    }
-
-    const find = (query, by) => {
-        RestaurantDataService.find(query, by).then(
-            response => {
-                console.log(response.data);
-                setRestaurants(response.data.restaurants)
-            }
-        )
-        .catch(e => {
-            console.log(e);
-        })
-    }
-
-    const findByName = () => {
-        find(searchName, "name")
-    }
-
-    const findByZip = () => {
-        find(searchZip, "zipcode")
-    }
-
-    const findByCuisine = () => {
-        if(searchCuisine == "All Cuisines") {
-            refreshList()
-        } else {
-            find(searchCuisine, "cuisine")
-        }
-    }
-
-    return (
-        <div>
-           <div className="row pb-1">
-               <div className="input-group col-lg-4">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Search by name"
-                        value={searchName}
-                        onChange={onChangeSearchName}
-                    />
-                    <div className="input-group-append">
-                        <button
-                            className="btn btn-outline-secondary"
-                            type="button"
-                            onClick={findByName}
-                        >
-                            Search
-                        </button>
-                    </div>
-               </div>
-               <div className="input-group col-lg-4">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Search by zip"
-                        value={searchZip}
-                        onChange={onChangeSearchZip}
-                    />
-                    <div className="input-group-append">
-                        <button
-                            className="btn btn-outline-secondary"
-                            type="button"
-                            onClick={findByZip}
-                        >
-                            Search
-                        </button>
-                    </div>
-               </div>
-               <div className="input-group col-lg-4">
-                    <select onChange={onChangeSearchCuisine}>
-                        {cuisines.localeCompare(cuisine => {
-                            return (
-                                <option value={cuisine}>{cuisine.substr(0, 20)}</option>
-                            )
-                        })}
-                    </select>
-                    <div className="input-group-append">
-                        <button
-                            className="btn btn-outline-secondary"
-                            type="button"
-                            onClick={findByCuisine}
-                        >
-                            Search
-                        </button>
-                    </div>
-               </div>
-           </div> 
-           <div className="row">
-                {restaurants.map((restaurant) => {
-                    const address = `${restaurant.address.building} ${restaurant.address.street}, ${restaurant.address.zipcode}`
-                    return (
-                        <div className="col-lg-4 pb-1">
-                            <div className="card">
-                                <div className="cord-body">
-                                    <h5 className="card-title">
-                                        {restaurant.name}
-                                    </h5>
-                                    <p className="card-text">
-                                        <strong>Cuisine :</strong>{restaurant.cuisine}<br/>
-                                        <strong>Addess :</strong>{address}
-                                    </p>
-                                    <div className="row">
-                                        <Link to={"/restaurants/"+restaurant._id} className="btn btn-primary col-lg-5 mx-1 mb-1">
-                                            View Reviews
-                                        </Link>
-                                        <a target="_blanck" href={"https://www.google.com/maps/place/" + address} className="btn btn-primary col-lg-5 mx-1 mb-1">
-                                            View Map
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
-           </div>
-        </div>
-    )
+   const deleteReview = (reviewId, index) => {
+       RestaurantDataService.deleteReview(reviewId).then(
+           response => {
+               setRestaurant((prevState) => {
+                   prevState.reviews.splice(index, 1)
+                   return({
+                       ...prevState
+                   })
+               })
+           }
+       )
+       .catch(e => {
+           console.log(e);
+       })
+   }
 }
 
 export default Restaurant
