17fadb0f00a22940c9d03e572f71f6f9c68ddeee
Tifenn <tifenn.queudet@gmail.com>
2021-05-27 12:11:47 +0200

    Restaurants List html render

--- frontend/src/components/Restaurant.js
+++ frontend/src/components/Restaurant.js
@@ -88,7 +88,90 @@ const Restaurant = (props) => {
 
     return (
         <div>
-           Restaurant 
+           <div className="row pb-1">
+               <div className="input-group col-lg-4">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Search by name"
+                        value={searchName}
+                        onChange={onChangeSearchName}
+                    />
+                    <div className="input-group-append">
+                        <button
+                            className="btn btn-outline-secondary"
+                            type="button"
+                            onClick={findByName}
+                        >
+                            Search
+                        </button>
+                    </div>
+               </div>
+               <div className="input-group col-lg-4">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Search by zip"
+                        value={searchZip}
+                        onChange={onChangeSearchZip}
+                    />
+                    <div className="input-group-append">
+                        <button
+                            className="btn btn-outline-secondary"
+                            type="button"
+                            onClick={findByZip}
+                        >
+                            Search
+                        </button>
+                    </div>
+               </div>
+               <div className="input-group col-lg-4">
+                    <select onChange={onChangeSearchCuisine}>
+                        {cuisines.localeCompare(cuisine => {
+                            return (
+                                <option value={cuisine}>{cuisine.substr(0, 20)}</option>
+                            )
+                        })}
+                    </select>
+                    <div className="input-group-append">
+                        <button
+                            className="btn btn-outline-secondary"
+                            type="button"
+                            onClick={findByCuisine}
+                        >
+                            Search
+                        </button>
+                    </div>
+               </div>
+           </div> 
+           <div className="row">
+                {restaurants.map((restaurant) => {
+                    const address = `${restaurant.address.building} ${restaurant.address.street}, ${restaurant.address.zipcode}`
+                    return (
+                        <div className="col-lg-4 pb-1">
+                            <div className="card">
+                                <div className="cord-body">
+                                    <h5 className="card-title">
+                                        {restaurant.name}
+                                    </h5>
+                                    <p className="card-text">
+                                        <strong>Cuisine :</strong>{restaurant.cuisine}<br/>
+                                        <strong>Addess :</strong>{address}
+                                    </p>
+                                    <div className="row">
+                                        <Link to={"/restaurants/"+restaurant._id} className="btn btn-primary col-lg-5 mx-1 mb-1">
+                                            View Reviews
+                                        </Link>
+                                        <a target="_blanck" href={"https://www.google.com/maps/place/" + address} className="btn btn-primary col-lg-5 mx-1 mb-1">
+                                            View Map
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+           </div>
         </div>
     )
 }
